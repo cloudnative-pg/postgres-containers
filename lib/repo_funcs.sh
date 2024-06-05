58becf0bd9436b36e6a24bb743e79329bdb7aa3d
Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
2024-06-05 13:28:33 +0200

    feat: add PostgreSQL 17beta1 images (#107)
    Adding documentation on the project's structure and on how to add a new Postgres Beta version.
    
    Signed-off-by: Gabriele Bartolini <gabriele.bartolini@enterprisedb.com>
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>
    Co-authored-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- lib/repo_funcs.sh
+++ lib/repo_funcs.sh
@@ -15,6 +15,9 @@
 # limitations under the License.
 #
 
+# Update this everytime a new major release of PostgreSQL is available
+POSTGRESQL_LATEST_MAJOR_RELEASE=16
+
 # Get the last postgres base image tag and update time
 fetch_postgres_image_version() {
 	local suite="$1"; shift
@@ -133,7 +136,7 @@ generate_postgres() {
 	fi
 
 	dockerTemplate="Dockerfile.template"
-	if [[ ${version} -gt "16" ]]; then
+	if [[ ${version} -gt "${POSTGRESQL_LATEST_MAJOR_RELEASE}" ]]; then
 		dockerTemplate="Dockerfile-beta.template"
 	fi
 
