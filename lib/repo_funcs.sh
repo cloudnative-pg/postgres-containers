943ccbbc4e0975bd80cbf581b573d82df90980a7
Niccolò Fei <niccolo.fei@enterprisedb.com>
2025-03-05 18:05:44 +0100

    feat: support additional barman-cloud compression methods in system images
    Signed-off-by: Niccolò Fei <niccolo.fei@enterprisedb.com>

--- lib/repo_funcs.sh
+++ lib/repo_funcs.sh
@@ -166,7 +166,7 @@ generate_postgres() {
 update_requirements() {
 	barmanVersion=$(get_latest_barman_version)
 	# If there's a new version we need to recreate the requirements files
-	echo "barman[cloud,azure,snappy,google] == $barmanVersion" > requirements.in
+	echo "barman[cloud,azure,snappy,google,zstandard,lz4] == $barmanVersion" > requirements.in
 	echo "boto3 == 1.35.99" >> requirements.in
 
 	# This will take the requirements.in file and generate a file
